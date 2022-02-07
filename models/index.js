e764724c5429a48de6c2affa60df8f7e8cc9d3c0
rwood022 <93175265+rwood022@users.noreply.github.com>
2022-02-07 11:01:08 -0500

    commented in models

--- models/index.js
+++ models/index.js
@@ -1,19 +1,20 @@
-// const Dogs = require('./Dogs');
-// const Owner = require('./Owner');
-// const PotentialOwner = require('./PotentialOwner');
+const Dogs = require('./Dogs');
+const Owner = require('./Owner');
+const PotentialOwner = require('./PotentialOwner');
 const Story = require("./Story");
-// PotentialOwner.hasMany(Dogs, {
-//     foreignKey: "potentialowner_id",
-//     onDelete: 'CASCADE'
-// });
+PotentialOwner.hasMany(Dogs, {
+    foreignKey: "potentialowner_id",
+    onDelete: 'CASCADE'
+});
 
-// Dogs.belongsTo(PotentialOwner, {
-//     foreignKey: "potentialowner_id"
-// });
+Dogs.belongsTo(PotentialOwner, {
+    foreignKey: "potentialowner_id"
+});
 
-// Owner.belongsTo(PotentialOwner, {
-//     foreignKey: "potentialowner_id",
-//     onDelete: 'Cascade'
-// });
+Owner.belongsTo(PotentialOwner, {
+    foreignKey: "potentialowner_id",
+    onDelete: 'Cascade'
+});
+
 // exporting models
-module.exports = { Story };
+module.exports = { PotentialOwner, Dogs, Owner, Story };
